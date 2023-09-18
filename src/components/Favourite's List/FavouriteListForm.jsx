05dde963d643088ca0eff28c946fb30ed8c8046c
ryu <cristellechoo@hotmail.com>
2023-09-18 10:23:09 +0800

    made FavouriteListForm and FavouriteListItem

--- src/components/Favourite's List/FavouriteListForm.jsx
+++ src/components/Favourite's List/FavouriteListForm.jsx
@@ -1,13 +1,26 @@
-
+import FavouriteListItem from "./FavouriteListItem"
 
 
 export default function FavouriteListForm() {
+    const tableStyle = {
+        "border": "2px solid black"
+     };
    return (
     <>
-    <form>
+    <form > 
+        <label>Add a New Bias!</label>
+        <br/>
         <input></input>
         <button>i choose you!</button>
     </form>
+        <table style={tableStyle}>
+            <thead>
+            <tr>
+                <th>Favourite's List</th>
+            </tr>
+            </thead>
+            <FavouriteListItem />
+        </table>
     </>
    )
 }
---
+++ src/components/Favourite's List/FavouriteListItem.jsx
@@ -0,0 +1,17 @@
+export default function FavouriteListItem() {
+    const tableStyle = {
+        "border": "1px solid black",
+        "textAlign": "center"
+     };
+
+    return(
+        <tbody>
+        <tr>
+            <td style={tableStyle}> 
+                yun
+            </td>
+        </tr>
+       
+        </tbody>
+    )
+}
