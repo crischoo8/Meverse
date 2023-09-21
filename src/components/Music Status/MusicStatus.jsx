9a2693cf4c565e012074e4db905e6845ed1ecf04
ryu <cristellechoo@hotmail.com>
2023-09-21 11:43:13 +0800

    able to fetch songs from api need to work on displaying it

--- src/components/Music Status/MusicStatus.jsx
+++ src/components/Music Status/MusicStatus.jsx
@@ -1,39 +1,67 @@
-
 import '../../Component Styles/Card.css'
+import { useState, useEffect } from 'react';
+
 export default function MusicStatus() {
+    const [music, setMusic] = useState({});
     const handleRefresh = function(event) {
         event.preventDefault();
      }
+    const addMusic = async function(event) {
+        event.preventDefault();
+        const url = 'https://k-pop.p.rapidapi.com/songs/random';
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '3a9180ad1fmsh49e80cc673aa29dp12e99djsn155fb5eeaa1f',
+                'X-RapidAPI-Host': 'k-pop.p.rapidapi.com'
+            }
+        };
+
+       
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+        setMusic(result);
+    
+        console.log(music);
+    }
     return(
         <>
         <div className="componentCard">
-           <h3 className="label">current bop</h3>
+           <h3 className="label">rando bop!</h3>
            <br />
-        <table className="table">
+        <table className="musictable">
             <tbody>
             <tr>
                 <td>
-                <strong>SONG:</strong> between you, me and the lampost
+                {/* <strong>SONG:</strong> {music.data[0].['Song Name']} */}
+                {JSON.stringify(music.data)}
                 </td>
             </tr>
 
             <tr>
                 <td>
-                <strong>ARTIST:</strong> LSFM
+                {/* <strong>ARTIST:</strong> {music.data[0]?.Artist} */}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                {/* <strong>LINK:</strong> <a href={music.data[0]?.Video} target="_blank">listen here</a> */}
                 </td>
             </tr>
             </tbody>
         </table>
         <br />
         <form>
-            
+{/*             
             <label><strong>update your song of the day here!</strong></label>
             <br />
             <input></input>
             <button onClick={handleRefresh}>update</button>
+            <br /> */}
+            <label style={{"fontSize":"18px"}}><strong><em>get your music rec here</em></strong></label>
             <br />
-            <label style={{"fontSize":"24px", "padding": "10px"}}><strong><em>feeling frisky?</em></strong></label>
-            <button onClick={handleRefresh}>random song!</button>
+            <button onClick={addMusic}>random song!</button>
             
         </form>
         </div>
